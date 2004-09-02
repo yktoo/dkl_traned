15e4961386bc257a51290c598b9b645e83ce491b
Dmitry Kann <yktooo@gmail.com>
2004-09-02 14:22:23 +0000

    DTLangTools полностью переименован в DKLang

--- TranEditor/udSelLang.pas
+++ TranEditor/udSelLang.pas
@@ -3,8 +3,8 @@ unit udSelLang;
 interface
 
 uses
-  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, ExtCtrls, DTLangTools;
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms, Dialogs,
+  StdCtrls, ExtCtrls;
 
 type
   TdSelLang = class(TForm)
--- TranEditor/udSettings.pas
+++ TranEditor/udSettings.pas
@@ -3,8 +3,8 @@ unit udSettings;
 interface
 
 uses
-  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, ExtCtrls, DTLangTools;
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms, Dialogs,
+  StdCtrls, ExtCtrls;
 
 type
   TdSettings = class(TForm)

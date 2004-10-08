dca4c54aa591584225c301f70b27d0982e3529d7
Dmitry Kann <yktooo@gmail.com>
2004-10-08 04:23:33 +0000

    Опубликован DKLang Translation Editor v2.1 Language Source

--- TranEditor/Install/make_distrib.bat
+++ TranEditor/Install/make_distrib.bat
@@ -1,6 +1,6 @@
 @echo off
 rem ********************************************************************************************************************
-rem $Id: make_distrib.bat,v 1.1.1.1 2004-09-26 17:15:05 dale Exp $
+rem $Id: make_distrib.bat,v 1.2 2004-10-08 04:23:33 dale Exp $
 rem --------------------------------------------------------------------------------------------------------------------
 rem DKLang Localization Package
 rem Copyright 2002-2004 DK Software, http://www.dk-soft.org/
@@ -9,9 +9,12 @@ rem ** Making bundle of the Translation Editor application
 
 set VERSION=2.1
 set OUT_FILE_NAME=dktraned-%VERSION%.zip
+set LANGSRC_FILE_NAME=dktraned-%VERSION%-LangSrc.zip
 
 if exist %OUT_FILE_NAME% del %OUT_FILE_NAME%
+if exist %LANGSRC_FILE_NAME% del %LANGSRC_FILE_NAME%
 
 rem -m3    = compression normal
 rem -afzip = create zip archive
 start /w C:\Progra~1\WinRAR\WinRAR.exe a -m3 -afzip %OUT_FILE_NAME% ..\DKTranEd.exe ..\Language\*.lng
+start /w C:\Progra~1\WinRAR\WinRAR.exe a -m3 -afzip %LANGSRC_FILE_NAME% ..\DKTranEd.dklang

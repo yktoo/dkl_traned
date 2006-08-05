f95a7530bfe4dca5351f1c4465b9e93861c377e8
Dmitry Kann <yktooo@gmail.com>
2006-08-05 21:37:25 +0000

    Added CHM help

---
+++ TranEditor/Help/aliases.h
@@ -0,0 +1,16 @@
+IDH_iface_dlg_about                   = iface-dlg-about.html
+IDH_iface_dlg_diff_log                = iface-dlg-diff-log.html
+IDH_iface_dlg_find                    = iface-dlg-find.html
+IDH_iface_dlg_open_files              = iface-dlg-open-files.html
+IDH_iface_dlg_prompt_replace          = iface-dlg-prompt-replace.html
+IDH_iface_dlg_settings                = iface-dlg-settings.html
+IDH_iface_dlg_tran_props              = iface-dlg-tran-props.html
+IDH_iface_main_menu                   = iface-main-menu.html
+IDH_iface_wnd_main                    = iface-wnd-main.html
+IDH_index                             = index.html
+IDH_main_contact_information          = main-contact-information.html
+IDH_main_installation                 = main-installation.html
+IDH_main_license_agreement            = main-license-agreement.html
+IDH_main_package_contents             = main-package-contents.html
+IDH_main_revision_history             = main-revision-history.html
+
---
+++ TranEditor/Help/maps.h
@@ -0,0 +1,23 @@
+//**********************************************************************************************************************
+//  $Id: maps.h,v 1.1 2006-08-05 21:37:25 dale Exp $
+//----------------------------------------------------------------------------------------------------------------------
+//  DKLang Translation Editor
+//  Copyright 2002-2006 DK Software, http://www.dk-soft.org/
+//**********************************************************************************************************************
+
+#define IDH_iface_dlg_about              0010
+#define IDH_iface_dlg_diff_log           0020
+#define IDH_iface_dlg_find               0030
+#define IDH_iface_dlg_open_files         0040
+#define IDH_iface_dlg_prompt_replace     0050
+#define IDH_iface_dlg_settings           0060
+#define IDH_iface_dlg_tran_props         0070
+#define IDH_iface_main_menu              0071 
+#define IDH_iface_wnd_main               0080
+#define IDH_index                        0090
+#define IDH_main_contact_information     0100
+#define IDH_main_installation            0110
+#define IDH_main_license_agreement       0120
+#define IDH_main_package_contents        0130
+#define IDH_main_revision_history        0140
+
